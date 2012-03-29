bfe170073a0e992f33d85fddaeaad351e4d1c510
Johan Berggren <jbn@nordu.net>
2012-03-29 09:52:32 +0200

    Removed cluster

--- meta/apps/metadata/views.py
+++ meta/apps/metadata/views.py
@@ -12,7 +12,7 @@ def index(request):
     institutions = Institution.objects.all()
     locations = Location.objects.all()
     ap_no = Location.objects.all().aggregate(Sum('ap_no'))['ap_no__sum']
-    return render_to_response("metadata/index.html", {'institutions': institutions, 'locations': locations, 'ap_no': ap_no}, RequestContext(request))
+    return render_to_response("index.html", {'institutions': institutions, 'locations': locations, 'ap_no': ap_no}, RequestContext(request))
 
 @login_required
 def institution(request):
